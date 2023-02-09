f43b0f3d3485b79156c1b3b1bb7eff341f122e30
Tran Minh Thien <tranthienrs24@gmail.com>
2023-02-09 11:36:58 +0700

    init firstdemo

---
+++ FirstDemo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        int a = 15;
+        int b = 10;
+        if (a>b){
+            System.out.printf("%d, %d", a, b);
+        } else System.out.println("b >= a");
+    }
+}
